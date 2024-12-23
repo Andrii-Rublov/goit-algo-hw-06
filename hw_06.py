9a909b0ceec67a31b307831fc962d77a1762fbb7
Andrii-Rublov <rublyovandrew@gmail.com>
2024-12-23 19:34:38 +0200

    Update hw_06.py

--- hw_06.py
+++ hw_06.py
@@ -9,12 +9,12 @@ class Field:
 
 class Name(Field):
      def __init__(self, name):
-        self.name = name
+        super().__init__(name)
 
 class Phone(Field):
     def __init__(self, value):
-        if len(value) != 10:
-            raise ValueError("Phone number must be a 10-digit format.")
+        if not value.isdigit() or len(value) != 10:     # Check for validity of input
+            raise ValueError
         super().__init__(value)
 
 
@@ -27,21 +27,21 @@ class Record:
         self.phones.append(Phone(phone))
 
     def edit_phone(self, old_phone, new_phone):
-        for i, p in enumerate(self.phones):
-            if p.value == old_phone:
-                try:                                    
-                    self.phones[i] = Phone(new_phone) 
-                    return True
-                except ValueError:
-                        print(f"Phone number must be a 10-digit format.")
-                        return False
-        return False                                    #  if old_phone is not found
-
+        p = self.find_phone(old_phone)
+        if p:                                       #  if the old phone was found, remove it and add new phone
+            self.remove_phone(old_phone)
+            self.add_phone(new_phone)
+        else:
+            raise ValueError                        #  if the old phone wasn't found
+            
     def remove_phone(self, phone):
-        self.phones = [p for p in self.phones if p.value != phone]
+        self.phones = [p for p in self.phones if p.value != phone]  
 
     def find_phone(self, phone):
-        return any(p.value == phone for p in self.phones)
+        for p in self.phones:
+            if p.value == phone:
+                return p                                # Return the matching Phone object
+        return None                                     # Return None if phone is not found
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -50,14 +50,13 @@ class AddressBook(UserDict):
     def add_record(self, record):                   # Add a Record item to the address book
         self.data[record.name.value] = record
 
-    def find_record(self, name):                    # Find and return a Record item by name
+    def find(self, name):                    # Find and return a Record item by name
         return self.data.get(name) 
 
-    def delete_record(self, name):                     # Delete a Record item by name
+    def delete(self, name):                     # Delete a Record item by name
         if name in self.data:
             del self.data[name]
-            return True
-        return False
+       
 
     def __str__(self):
         return "\n".join([str(record) for record in self.data.values()]) #output of records
